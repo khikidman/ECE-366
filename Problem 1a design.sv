48bcbf00c22c36fe0bed88818e007283904e15b4
bpand5 <bpand5@uic.edu>
2026-09-23 14:39:41 -0500

    Update Problem 1a design.sv
    behavioral

--- Problem 1a design.sv
+++ Problem 1a design.sv
@@ -1,10 +1,10 @@
 module one_bit_full_adder(A, B, Cin, S, Cout);
-  
-input A, B, Cin;
-output S, Cout;
- 
-assign Cout = (A&B) | (Cin&(A^B));
-assign S = (A^B)^Cin;
 
-  
+  input A, B, Cin;
+  output reg S, Cout;
+
+  always @(*) begin
+    {Cout, S} = {1'b0, A} + {1'b0, B} + {1'b0, Cin};
+  end
+
 endmodule
